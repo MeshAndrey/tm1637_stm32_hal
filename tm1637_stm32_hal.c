d36d29e0fcb679d13c222a2829e9ce58f90f2b04
MeshAndrey <35770825+MeshAndrey@users.noreply.github.com>
2021-08-18 22:05:48 +0300

    fix spacings

--- tm1637_stm32_hal.c
+++ tm1637_stm32_hal.c
@@ -22,22 +22,22 @@ void tm1637SetBrightness(uint8_t brightness);
 
 const uint8_t segmentMap[] = {
     0x3f, // 0d
-	0x06, // 1d
-	0x5b, // 2d
-	0x4f, // 3d
-	0x66, // 4d 
-	0x6d, // 5d
-	0x7d, // 6d
-	0x07, // 7d
+    0x06, // 1d
+    0x5b, // 2d
+    0x4f, // 3d
+    0x66, // 4d 
+    0x6d, // 5d
+    0x7d, // 6d
+    0x07, // 7d
     0x7f, // 8d
-	0x6f, // 9d
-	0x77, // A
-	0x7c, // b
-	0x39, // C
-	0x5e, // d
-	0x79, // E
-	0x71, // F
-	0x50, // r
+    0x6f, // 9d
+    0x77, // A
+    0x7c, // b
+    0x39, // C
+    0x5e, // d
+    0x79, // E
+    0x71, // F
+    0x50, // r
     0x00  // EMPTY
 };
 
@@ -85,12 +85,12 @@ void tm1637DisplayDecimal(uint32_t v, uint8_t displaySeparator)
 {
     uint8_t digitArr[4];
     for (uint8_t i = 0; i < 4; ++i) 
-	{
+    {
         digitArr[i] = segmentMap[v % 10];
         if (i == 2 && displaySeparator == 1) // if have separator
-		{	
-            digitArr[i] |= 1 << 7;	// set older bit as 1 
-										//and bitwise OR with digit
+        {
+            digitArr[i] |= 1 << 7;
+
         }
         v /= 10;
     }
@@ -105,7 +105,7 @@ void tm1637DisplayDecimal(uint32_t v, uint8_t displaySeparator)
     _tm1637ReadResult();
 
     for (uint8_t i = 0; i < 4; ++i) 
-	{
+    {
         _tm1637WriteByte(digitArr[3 - i]);
         _tm1637ReadResult();
     }
@@ -136,44 +136,44 @@ void _tm1637ReadResult(void)
 
 void _tm1637WriteByte(uint8_t b)
 {
-	/*
-	for example b = 0b1001 (9d):
-		1 step - 0b00001001 & 0b00000001 = 1 => write 1 and right shift
-		2 step - 0b00000100 & 0b00000001 = 0 => write 0 and right shift
-		3 step - 0b00000010 & 0b00000001 = 0 => write 0 and right shift
-		4 step - 0b00000001 & 0b00000001 = 1 => write 1 and right shift
-	 */
-	
+    /*
+    for example b = 0b1001 (9d):
+        1 step - 0b00001001 & 0b00000001 = 1 => write 1 and right shift
+        2 step - 0b00000100 & 0b00000001 = 0 => write 0 and right shift
+        3 step - 0b00000010 & 0b00000001 = 0 => write 0 and right shift
+        4 step - 0b00000001 & 0b00000001 = 1 => write 1 and right shift
+    */
+
     for (uint8_t i = 0; i < 8; ++i) // a byte cycle 
-	{
+    {
         _tm1637ClkLow();
-		
-        if (b & 0x01) 				// if bit is 1 		
-            _tm1637DioHigh();		// write 1        
+
+        if (b & 0x01)         // if bit is 1
+            _tm1637DioHigh(); // write 1
         else  
-			_tm1637DioLow();		// else write 0        
-		
+            _tm1637DioLow();  // else write 0
+
         _tm1637DelayUsec(3);
-		
-        b >>= 1;					// right bit shift for 1 
-		
+
+        b >>= 1;              // right bit shift for 1 
+
         _tm1637ClkHigh();
         _tm1637DelayUsec(3);
     }
 }
-	
+
 void _tm1637DelayUsec(uint32_t i)
 {
     for (; i>0; i--) 
-	{
+    {
         for (int j = 0; j < 10; ++j) 
-		{
-            __asm	// a syntax for arm compiler 
-					// for gcc - another syntax
-					// should be like this : __asm__ __volatile__("nop\n\t":::"memory");
-			{
-				nop
-			}
+        {
+            __asm  // a syntax for arm compiler 
+                   // for gcc - another syntax
+                   // should be like this : __asm__ __volatile__("nop\n\t":::"memory");
+            {
+                nop
+            }
         }
     }
 }
